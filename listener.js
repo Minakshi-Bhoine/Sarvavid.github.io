6d355942560700c8c97365825ebdc228e77cee65
abhijits3 <37706733+abhijits3@users.noreply.github.com>
2019-03-21 17:31:03 +0530

    Update listener.js

--- listener.js
+++ listener.js
@@ -24,7 +24,7 @@ function connectMQTT() {
 
 function onConnect() {
     console.log("Connected to server");
-    mqClient.subscribe(topic);
+    client.subscribe(topic);
    }
    
 function onMessageArrived(message) {
