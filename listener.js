ea3087566ad0df4a3c1a927352ce6bebf22a2629
abhijits3 <37706733+abhijits3@users.noreply.github.com>
2019-03-21 16:50:22 +0530

    Create listener.js

---
+++ listener.js
@@ -0,0 +1,45 @@
+var ip = "mqtt.altairsmartcore.com";
+var port = "8884";
+var usessl = false;
+var id = (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+var username = 'smartDevice.SmartDevice@sarvavid.sarvavid';
+var password = 'smart123';
+var topic = 'SmartDevice@sarvavid.sarvavid/Data';
+var message, client;
+var connected = false;
+
+function connectMQTT() {
+    client = new Paho.MQTT.Client(ip, Number(port), id);
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    client.connect({
+        userName: username,
+        password: password,
+        useSSL: usessl,
+        onSuccess: onConnect,
+        onFailure: onFailure,
+        reconnect: true
+    });
+}
+
+function onConnect() {
+    console.log("Connected to server");
+    mqClient.subscribe(topic);
+   }
+   
+function onMessageArrived(message) {
+try {
+    console.log("Recieved Message from server");
+    var value = message.payloadString;
+    console.log("value: " + value);
+    } catch (e) {
+        console.log("exception in onMessageArrived: " + e);
+        return false;
+    }
+}
+
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+}
